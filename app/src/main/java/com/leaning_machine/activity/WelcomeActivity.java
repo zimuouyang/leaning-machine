ac56364a9a96f1cf9cf7b76a85178c7c806c6863
zhaoshuailei <zhaoshuailei@paxsz.com>
2021-09-13 20:13:41 +0800

    First commit

---
+++ app/src/main/java/com/leaning_machine/activity/WelcomeActivity.java
@@ -0,0 +1,19 @@
+package com.leaning_machine.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.leaning_machine.R;
+
+/**
+ * @author John
+ */
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
