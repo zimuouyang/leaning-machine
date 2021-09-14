959230ece529a3f152d3ddc5859d13d0441bb8bb
john <john>
2021-09-15 01:34:08 +0800

    根据包名跳转

--- app/src/main/java/com/leaning_machine/fragment/BaseFragment.java
+++ app/src/main/java/com/leaning_machine/fragment/BaseFragment.java
@@ -1,7 +1,10 @@
 package com.leaning_machine.fragment;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
@@ -9,6 +12,12 @@ public abstract class BaseFragment extends Fragment {
     public abstract void initView(View view);
 
     public void openApp(String packageName, Context context) {
-
+        PackageManager packageManager = context.getPackageManager();
+        Intent intent = packageManager.getLaunchIntentForPackage(packageName);
+        if (intent == null) {
+            Toast.makeText(context, "未安装", Toast.LENGTH_LONG).show();
+        } else {
+            context.startActivity(intent);
+        }
     }
 }
